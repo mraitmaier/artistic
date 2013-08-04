a24b3f4682a6e0b6ad50b45c5511e88c70d03d00
miranr <miranr@localhost>
2013-08-04 16:00:29 +0200

    A new and shorter version (and much much better) version using bcrypt algorithm.

--- common/password.go
+++ common/password.go
@@ -2,15 +2,15 @@
  * password.go 
  *
  * History:
- *  0.1   Jun11 MR Initial version, limited testing
+ *  1   Jun11 MR Initial version, limited testing
+ *  2   Aug13 MR New and much shorter version using bcrypt package
  */
 
 package artistic
 
 import (
-	"crypto/md5"
 	"fmt"
-	"math/rand"
+    "code.google.com/p/go.crypto/bcrypt"
 )
 
 /*
@@ -30,81 +30,25 @@ type Password struct {
 /*
  * Password.Set - set a password
  *
- * Password will, of course, be hashed.
+ * Password will, of course, be hashed using bcrypt algorithm.
  */
-func (p *Password) Set(passwd string) { p.pwd = p.hashPwd(passwd) }
+func (p *Password) Set(passwd string) (e error) {
+    p.pwd, e = bcrypt.GenerateFromPassword([]byte(passwd), 0)
+    return e
+}
 
 /*
  * Password.Get - return a stored hashed password
  */
-func (p *Password) Get() string { return fmt.Sprintf("%x", (p.pwd)) }
+func (p *Password) Get() string { return fmt.Sprintf("%x", p.pwd) }
 
 /*
  * Password.Cmp - compare arbitrary password to the one stored 
  */
-func (p *Password) Cmp(passwd string) bool {
-	hashed := p.removeSalt(p.hashPwd(passwd))
-	stored := p.removeSalt(p.pwd)
-	// we compare two []byte: lengths must be the same and every value  of the
-	// buffer must the same
-	if len(hashed) != len(stored) {
-		return false
-	}
-	for cnt, val := range hashed {
-		if val != stored[cnt] {
-			return false
-		}
-	}
-	return true
+func (p *Password) Cmp(toCompare string) bool {
+    status := true
+    e := bcrypt.CompareHashAndPassword(p.pwd, []byte(toCompare))
+    if e != nil { status = false }
+    return status
 }
 
-/*
- * Password.addSalt - add a 'salt' to password hashing function
- *
- * Salt is a series of bytes that are appended to hashed password. The reason 
- * is, of course, that password is even harder to crack. 
- * Note that this operation is defined as a method; there's a simple reason: we
- * don't want this operation to be used elsewhere in the code. It is bound to
- * Password type and that's it.
- */
-const saltSize int = 8
-
-func (p *Password) addSalt() []byte {
-	var b [saltSize]byte
-	for cnt := 0; cnt < saltSize; cnt++ {
-		b[cnt] = byte(rand.Intn(255))
-	}
-	return b[:]
-}
-
-/*
- * Password.removeSalt - returns the hashed password without the 'salt' part
- *
- * Note that this operation is defined as a method; there's a simple reason: we
- * don't want this operation to be used elsewhere in the code. It is bound to
- * Password type and that's it.
- */
-func (p *Password) removeSalt(saltedPwd []byte) []byte {
-	return saltedPwd[saltSize:]
-}
-
-/*
- * Password.hashPwd - hash a string password using MD5
- *
- * Note that this operation is defined as a method; there's a simple reason: we
- * don't want this operation to be used elsewhere in the code. It is bound to
- * Password type and that's it.
- */
-const numOfHashIteration int = 1017
-
-func (p *Password) hashPwd(passwd string) []byte {
-	h := md5.New()
-	// we need to convert a string into slice of bytes
-	b := []byte(passwd)
-	// let's iterate the MD5 hash more than a 1000-times
-	for cnt := 0; cnt < numOfHashIteration; cnt++ {
-		h.Write(b)
-		b = h.Sum(nil)
-	}
-	return append(p.addSalt(), b...)
-}
